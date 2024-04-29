f4ed26847314307451f624cd17a03d382dfe30e4
Ruan Jansen <ruan.d.jansen@gmail.com>
2024-04-29 19:14:58 +0200

    Converted ExampleApp to SwiftUI
    - Removed UIKit App delegate
    - Removed Storyboards
    - Added SwiftUI App delegate

---
+++ Example/SwiftUIGameKit/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleApp.swift
+//  SwiftUIGameKit_Example
+//
+//  Created by Ruan Jansen on 2024/04/29.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DashboardView()
+        }
+    }
+}
